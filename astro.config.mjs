7d72d63720f389f86e8b772282f5add2be5f8951
Данил Пельц <pelts2002@mail.ru>
2026-02-23 20:06:18 +0500

    Update resume

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
+// // @ts-check
+// import { defineConfig } from "astro/config";
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://astro.build/config
+// export default defineConfig({
+//   vite: {
+//     plugins: [tailwindcss()],
+//   },
+// });
+
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
+// Получаем имя репозитория для base
+const repoName = "/resume/"; // <- поменяй на имя репозитория на GitHub Pages
+
 export default defineConfig({
+  base: repoName,
   vite: {
     plugins: [tailwindcss()],
   },
